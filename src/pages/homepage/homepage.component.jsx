9ba690bfdb43e2b1f2b0902e75b9a34ee976f140
Erioluwa Bamigbola <erioluwabamigbola@gmail.com>
2021-05-07 19:23:59 +0100

    broken our homepage into smaller components, finished styling our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.styles.scss';
+import '../../components/directory/directory.component.jsx';
+import Directory from '../../components/directory/directory.component.jsx';
+
+
+const HomePage = () => (
+    <div className='homepage' >
+        <Directory />
+        </div>
+) 
+
+export default HomePage;
